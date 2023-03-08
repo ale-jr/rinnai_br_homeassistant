9ff33e6cddfdaf420fb636ab26193b4c5febda3e
Alexandre Fernandes Junior <alejr.dev@gmail.com>
2023-03-08 20:58:43 -0300

    Revert "Arrumar título da entidade"

--- modulo_aquecedor_rinnai_brasil/src/entities.js
+++ modulo_aquecedor_rinnai_brasil/src/entities.js
@@ -60,7 +60,7 @@ const waterTargetTemperature = createEntity('number', 'target_water_temperature'
     min: 35,
     max: 60,
     mode: 'box',
-    name: 'Temperatura definida',
+    name: 'Temperatura definiktda',
     optimistic: true,
     step: 1,
     unit_of_measurement: '°C'
